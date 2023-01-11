826f3aa3e751dfce03009b2ea3f9ce0d1d3b0870
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-01-11 09:52:46 +0100

    Initial Commit

---
+++ src/main/java/com/example/spring_schulung_aop/SpringSchulungAopApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_schulung_aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchulungAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSchulungAopApplication.class, args);
+	}
+
+}
